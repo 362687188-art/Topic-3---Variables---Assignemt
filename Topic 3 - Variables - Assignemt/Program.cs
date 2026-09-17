ce7bd859bbfa88612360d2c63b76e2f5061d827f
Teddy Becker <362687188@GoTVDSB.ca>
2026-09-17 12:42:09 -0400

    Add project files.

---
+++ Topic 3 - Variables - Assignemt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_3___Variables___Assignemt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
