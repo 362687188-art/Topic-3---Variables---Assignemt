de8c3ea66d3072c75e456897134a04a84795e00a
Teddy Becker <362687188@GoTVDSB.ca>
2026-09-17 13:20:15 -0400

    almost done assignment

--- Topic 3 - Variables - Assignemt/Program.cs
+++ Topic 3 - Variables - Assignemt/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Teddy
+            String learning = "I am learning a bit about Computer Science", firstName, lastName;
+            firstName = "Teddy";
+            lastName = "Becker";
+            int room, gradYear;
+            room = 29;
+            gradYear = 2028;
+            const double Pi = 3.14, Radius = 5;
+            double item1Price = 2.99, area = Pi * Radius * Radius;
+            
+
+            Console.WriteLine("This is room " + room);
+            Console.WriteLine("The price is " + item1Price);
+            Console.WriteLine(learning);
+
+            Console.WriteLine("My name is " + firstName + " " + lastName + " and I'll graduate in " + gradYear + ".");
+            Console.WriteLine("The area of this circle is " + area);   
+
         }
     }
 }
